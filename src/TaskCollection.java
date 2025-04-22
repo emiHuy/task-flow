4ddbd1f4fdd21958c427ada506c18ad65eda055c
emiHuy <emilyhuynh06@gmail.com>
2025-04-22 11:46:52 -0400

    update model

--- src/TaskCollection.java
+++ src/TaskCollection.java
@@ -1,19 +1,28 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class TaskCollection {
     private TreeSet<Task> tasks;
     private int tasksCompleted;
+    private TreeSet<String> categories;
 
     public TaskCollection() {
         tasks = new TreeSet<Task>();
         tasksCompleted = 0;
+        categories = new TreeSet<String>();
+        categories.add("Miscellaneous");
     }
 
     public TreeSet<Task> getTasks() {
-        return new TreeSet<Task>(tasks);
+        return tasks;
     }
     public int getTasksCompleted() {
-        return tasksCompleted;}
+        return tasksCompleted;
+    }
+    public TreeSet<String> getCategories() {
+        return categories;
+    }
 
     public boolean addTask(Task task) {
         if (tasks.add(task)) {
@@ -45,9 +54,37 @@ public class TaskCollection {
         t.setPriority(priority);
     }
 
-    public static TaskCollection example() {
-        TaskCollection taskCollection = new TaskCollection();
-        taskCollection.addTask(new Task("Study", 1));
-        return taskCollection;
+    public boolean addCategory(String category) {
+        return categories.add(category);
+    }
+
+    public ArrayList<Task> getTasksByDate(LocalDate date) {
+        ArrayList<Task> tasksOnDate = new ArrayList<Task>();
+        for (Task t: tasks) {
+            if (t.getDate().equals(date)) {
+                tasksOnDate.add(t);
+            }
+        }
+        return tasksOnDate;
+    }
+
+    public ArrayList<Task> getTasksByPriority(int priority) {
+        ArrayList<Task> tasksWithPriority= new ArrayList<Task>();
+        for (Task t: tasks) {
+            if (t.getPriority() == priority) {
+                tasksWithPriority.add(t);
+            }
+        }
+        return tasksWithPriority;
+    }
+
+    public ArrayList<Task> getTasksByCategory(String category) {
+        ArrayList<Task> tasksInCategory= new ArrayList<Task>();
+        for (Task t: tasks) {
+            if (t.getCategory().equals(category)) {
+                tasksInCategory.add(t);
+            }
+        }
+        return tasksInCategory;
     }
 }
